2556fb8af7dc6e0cd5267c434d8cf5370bacf7e0
CHOHYEON1 <chgus227@naver.com>
2023-03-17 14:09:31 +0900

    1

---
+++ src/managesystem/manage1.java
@@ -0,0 +1,13 @@
+package managesystem;
+
+public class manage1 {
+
+	public static void main(String[] args) {
+		System.out.println("1. add student");
+		System.out.println("2. delete student");
+		System.out.println("3. veiw student");
+		System.out.println("4. edit student");	
+		System.out.println("5. exit");
+	}
+
+}
