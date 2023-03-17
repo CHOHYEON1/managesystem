9e0220a82f257b4d35c4e89f4556c4cb94acad19
CHOHYEON1 <chgus227@naver.com>
2023-03-17 15:43:21 +0900

    1

--- src/managesystem/manage1.java
+++ src/managesystem/manage1.java
@@ -3,10 +3,10 @@ package managesystem;
 public class manage1 {
 
 	public static void main(String[] args) {
-		System.out.println("1. add student");
-		System.out.println("2. delete student");
-		System.out.println("3. veiw student");
-		System.out.println("4. edit student");	
+		System.out.println("1. add music");
+		System.out.println("2. delete music");
+		System.out.println("3. edit music imformation");
+		System.out.println("4. view music imformation");	
 		System.out.println("5. exit");
 	}
 
