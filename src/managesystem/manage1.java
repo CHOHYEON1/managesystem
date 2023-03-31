661fb9d29c690759817f673eb9f1bc4265f2a6f3
CHOHYEON1 <chgus227@naver.com>
2023-03-31 17:46:24 +0900

    method

--- src/managesystem/manage1.java
+++ src/managesystem/manage1.java
@@ -9,32 +9,67 @@ public class manage1 {
 		Scanner input = new Scanner(System.in);
 		
 		while(num !=6) {
+			System.out.println("**Music Library Managemant System**");
 			System.out.println("1. add music");
 			System.out.println("2. delete music");
 			System.out.println("3. edit music imformation");
 			System.out.println("4. view music imformation");	
 			System.out.println("5. show a menu");
 			System.out.println("6. exit");
+			System.out.println("Select one number between 1-6");
 			num = input.nextInt();
+			
 			switch(num) {
 				case 1: 
-					System.out.print("Music name: ");
-					String musicname = input.next();
-					System.out.print("Singer: ");
-					String Singer = input.next();
-					System.out.print("Release year: ");
-					int Releaseyear = input.nextInt();
-					System.out.print("Genre: ");
-					String Genre = input.next();
+					addmusic();
 					break;
 				case 2: 
+					deletemusic();
+					break;
 				case 3: 
+					editmusic();
+					break;
 				case 4: 
-					System.out.print("music name: ");
-					String musicname2 = input.next();
+					viewmusic();
+					break;
 				
 			}
 		}
+	}
+	
+	
+	public static void addmusic() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Music name: ");
+		String musicname = input.next();
+		System.out.print("Singer: ");
+		String Singer = input.next();
+		System.out.print("Release year: ");
+		int Releaseyear = input.nextInt();
+		System.out.print("Genre: ");
+		String Genre = input.next();
+		
+	}
+	
+	
+	
+	public static void deletemusic() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Music name: ");
+		String musicname = input.next();
+	}
+	
+	
+	public static void editmusic() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Music name: ");
+		String musicname = input.next();
+	}
+	
+	public static void viewmusic() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Music name: ");
+		String musicname = input.next();
 	}
 
 }
