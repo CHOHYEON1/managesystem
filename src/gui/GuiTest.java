7616198e16c4bf8155b3406d7bb8e5a5c9e0f398
CHOHYEON1 <chgus227@naver.com>
2023-06-02 19:32:20 +0900

    new

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -5,7 +5,7 @@ public class GuiTest {
 	public static void main(String[] args) {
 		//MenuSelection menuselection = new MenuSelection();
 		//MusicAdder musicadder = new MusicAdder();
-		MusicViewer musicviewer= new MusicViewer();
+		//MusicViewer musicviewer= new MusicViewer();
 	}
 
 }
